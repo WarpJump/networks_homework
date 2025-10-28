edca8328fc45b8c337de8de52ad14ec502d8c364
Danila Gushchin <warpjump@bk.ru>
2025-10-28 18:09:38 +0300

    rework

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -29,23 +29,24 @@ void error(const char *msg) {
   exit(EXIT_FAILURE);
 }
 
-SocketRAII setup_server_socket(int port, int type) {
+SocketRAII setup_server_socket(const char *interface, int port, int type) {
   SocketRAII server_fd(socket(AF_INET, type, 0));
   if (server_fd < 0)
     error("skill issue");
-
   if (type == SOCK_STREAM) {
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
       error("skill issue");
   }
-
   sockaddr_in address{};
   address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(port);
-
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+  address.sin_addr.s_addr = INADDR_ANY;
+  if (interface && strcmp(interface, "0.0.0.0") != 0) {
+    if (inet_pton(AF_INET, interface, &address.sin_addr) <= 0)
+      error("invalid interface address");
+  }
+  if (bind(server_fd, (sockaddr *)&address, sizeof(address)) < 0)
     error("skill issue");
   return server_fd;
 }
@@ -55,11 +56,9 @@ SocketRAII setup_client_socket(int type, const char *host, int port,
   SocketRAII sock_fd(socket(AF_INET, type, 0));
   if (sock_fd < 0)
     error("skill issue");
-
   serv_addr = {};
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(port);
-
   if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0)
     error("invalid address");
   return sock_fd;
@@ -69,64 +68,85 @@ void handle_tcp_client(SocketRAII client_socket) {
   std::cout << "Handling client on socket " << static_cast<int>(client_socket)
             << std::endl;
   char buffer[25000];
-  while (true) {
-    ssize_t bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-    if (bytes == 0) {
-      std::cout << "Client disconnected.\n";
-      break;
-    }
-    if (bytes < 0) {
-      std::cout << "Recv failed.\n";
-      break;
-    }
-    buffer[bytes] = '\0';
-    std::cout << "Client: " << buffer << std::flush;
+  std::string accumulated;
 
-    std::cout << "Server> ";
-    std::string reply;
-    if (!std::getline(std::cin, reply))
+  while (true) {
+    ssize_t bytes = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (bytes <= 0) {
+      std::cout << (bytes == 0 ? "Client disconnected." : "Recv failed.")
+                << std::endl;
       break;
-    reply += "\n";
-    if (send(client_socket, reply.c_str(), reply.length(), 0) < 0)
+    }
+    accumulated.append(buffer, bytes);
+    if (accumulated.size() > 1e7) {
+      std::cout << "Client data too large, closing." << std::endl;
       break;
+    }
+
+    size_t pos;
+    bool end_session = false;
+    while ((pos = accumulated.find('\n')) != std::string::npos &&
+           end_session == false) {
+      std::string msg = accumulated.substr(0, pos + 1);
+      accumulated.erase(0, pos + 1);
+      std::cout << "Client: " << msg << std::flush;
+      if (msg == "exit\n")
+        end_session = true;
+
+      std::cout << "Server> ";
+      std::string reply;
+      if (!std::getline(std::cin, reply))
+        end_session = true;
+      reply += "\n";
+      if (send(client_socket, reply.c_str(), reply.length(), 0) < 0) {
+        end_session = true;
+      }
+    }
+    if (end_session) {
+      break;
+    }
   }
   std::cout << "Connection closed. Waiting for new client..." << std::endl;
 }
 
-void run_tcp_server(int port) {
-  SocketRAII server_fd = setup_server_socket(port, SOCK_STREAM);
-  if (listen(server_fd, 5) < 0)
-    error("skill issue");
-  std::cout << "TCP Server listening on port " << port << std::endl;
+void print_server_start(const char *proto, const char *iface, int port) {
+  const char *listen_addr = (iface && strcmp(iface, "0.0.0.0") != 0)
+                                ? iface
+                                : "all interfaces (0.0.0.0)";
+  std::cout << proto << " Server listening on " << listen_addr << " port "
+            << port << std::endl;
+}
 
+void run_tcp_server(const char *interface, int port) {
+  SocketRAII server_fd = setup_server_socket(interface, port, SOCK_STREAM);
+  if (listen(server_fd, 5) < 0)
+    error("listen failed");
+  print_server_start("TCP", interface, port);
   while (true) {
     SocketRAII client_socket(accept(server_fd, NULL, NULL));
-    if (client_socket < 0) {
-      perror("skill issue");
-    } else {
+    if (client_socket < 0)
+      perror("accept failed");
+    else
       handle_tcp_client(std::move(client_socket));
-    }
   }
 }
 
 void run_tcp_client(const char *host, int port) {
   sockaddr_in serv_addr;
   SocketRAII sock = setup_client_socket(SOCK_STREAM, host, port, serv_addr);
-
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  if (connect(sock, (sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     error("connection failed");
   std::cout << "Connected to TCP server " << host << ":" << port << std::endl;
-
   char buffer[25000];
   while (true) {
     std::cout << "Client> ";
     std::string message;
-    if (!std::getline(std::cin, message))
+    if (!std::getline(std::cin, message) || message == "exit")
       break;
     message += "\n";
-    if (send(sock, message.c_str(), message.length(), 0) < 0)
+    if (send(sock, message.c_str(), message.length(), 0) < 0) {
       break;
-
+    }
     ssize_t bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
     if (bytes == 0) {
       std::cout << "Server disconnected.\n";
@@ -141,24 +161,22 @@ void run_tcp_client(const char *host, int port) {
   }
 }
 
-void run_udp_server(int port) {
-  SocketRAII sockfd = setup_server_socket(port, SOCK_DGRAM);
-  std::cout << "UDP Server listening on port " << port << std::endl;
-
-  char buffer[1024];
+void run_udp_server(const char *interface, int port) {
+  SocketRAII sockfd = setup_server_socket(interface, port, SOCK_DGRAM);
+  print_server_start("UDP", interface, port);
+  char buffer[65507];
   sockaddr_in cliaddr;
   socklen_t len = sizeof(cliaddr);
   while (true) {
-    ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
-                         (sockaddr *)&cliaddr, &len);
-    if (n < 0)
+    ssize_t n =
+        recvfrom(sockfd, buffer, sizeof(buffer), 0, (sockaddr *)&cliaddr, &len);
+    if (n < 0) {
       continue;
+    }
     buffer[n] = '\0';
-
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &cliaddr.sin_addr, client_ip, INET_ADDRSTRLEN);
     std::cout << "Client (" << client_ip << "): " << buffer << std::endl;
-
     const char *reply = "Message received";
     sendto(sockfd, reply, strlen(reply), 0, (sockaddr *)&cliaddr, len);
   }
@@ -167,31 +185,32 @@ void run_udp_server(int port) {
 void run_udp_client(const char *host, int port) {
   sockaddr_in servaddr;
   SocketRAII sockfd = setup_client_socket(SOCK_DGRAM, host, port, servaddr);
+  if (connect(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    error("udp connect failed");
   std::cout << "UDP Client sending to " << host << ":" << port << std::endl;
-
-  char buffer[1024];
+  char buffer[65507];
   while (true) {
     std::cout << "Client> ";
     std::string message;
     if (!std::getline(std::cin, message) || message == "exit")
       break;
-
-    sendto(sockfd, message.c_str(), message.length(), 0, (sockaddr *)&servaddr,
-           sizeof(servaddr));
-
-    ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
-    if (n < 0)
+    if (send(sockfd, message.c_str(), message.length(), 0) < 0) {
+      break;
+    }
+    ssize_t n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+    if (n < 0) {
       continue;
+    }
     buffer[n] = '\0';
-    std::cout << "Server: " << buffer << std::endl;
+    std::cout << "Server: " << buffer << std::flush;
   }
 }
 
-void usage(const char *program_name) {
+void usage(const char *name) {
   std::cerr << "Usage:\n"
-            << "  server: " << program_name << " server <tcp|udp> <port>\n"
-            << "  client: " << program_name
-            << " client <tcp|udp> <host> <port>\n";
+            << "  server: " << name
+            << " server <tcp|udp> <port> [interface_ip]\n"
+            << "  client: " << name << " client <tcp|udp> <host_ip> <port>\n";
   exit(EXIT_FAILURE);
 }
 
@@ -202,16 +221,20 @@ int main(int argc, char const *argv[]) {
   std::string mode = argv[1];
   std::string protocol = argv[2];
 
-  if (mode == "server" && argc == 4) {
-    int port = std::atoi(argv[3]);
-    if (protocol == "tcp") {
-      run_tcp_server(port);
-    } else if (protocol == "udp") {
-      run_udp_server(port);
-    } else {
+  if (mode == "server") {
+    if (argc < 4 || argc > 5)
       usage(argv[0]);
-    }
-  } else if (mode == "client" && argc == 5) {
+    int port = std::atoi(argv[3]);
+    const char *interface = (argc == 5) ? argv[4] : nullptr;
+    if (protocol == "tcp")
+      run_tcp_server(interface, port);
+    else if (protocol == "udp")
+      run_udp_server(interface, port);
+    else
+      usage(argv[0]);
+  } else if (mode == "client") {
+    if (argc != 5)
+      usage(argv[0]);
     const char *host = argv[3];
     int port = std::atoi(argv[4]);
     if (protocol == "tcp")
